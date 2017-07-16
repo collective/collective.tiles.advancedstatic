1c1fa1261804276ed9d672533cdfc2dc2a7404c6
Mauro Amico <mauro.amico@gmail.com>
2017-07-16 18:56:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
             # plone_coredev tests as of 2016-04-01.
             'plone.testing>=5.0.0',
             'plone.app.contenttypes',
+            'plone.app.tiles',
             'plone.app.robotframework[debug]',
         ],
     },
