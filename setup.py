40ca3058d7d98b167cd78a1e7334a69496f05dae
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-09-13 17:02:29 +0200

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.tiles.advancedstatic',
-    version='1.0a2',
+    version='1.0a3.dev0',
     description="Advanced static tile for plone mosaic",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
