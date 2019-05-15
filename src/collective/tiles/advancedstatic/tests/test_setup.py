cb440e0c877af52ea1104bee2468eff99e0c20ec
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-05-15 18:07:14 +0200

    fix travis

--- src/collective/tiles/advancedstatic/tests/test_setup.py
+++ src/collective/tiles/advancedstatic/tests/test_setup.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from collective.tiles.advancedstatic.testing import COLLECTIVE_TILES_ADVANCEDSTATIC_INTEGRATION_TESTING  # noqa
+from collective.tiles.advancedstatic.testing import (
+    COLLECTIVE_TILES_ADVANCEDSTATIC_INTEGRATION_TESTING,
+)  # noqa
 from plone import api
 
 import unittest
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
 
 
 class TestSetup(unittest.TestCase):
@@ -14,19 +21,29 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
 
     def test_product_installed(self):
         """Test if collective.tiles.advancedstatic is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'collective.tiles.advancedstatic'))
+        self.assertTrue(
+            self.installer.isProductInstalled(
+                'collective.tiles.advancedstatic'
+            )
+        )
 
     def test_browserlayer(self):
         """Test that ICollectiveTilesAdvancedstaticLayer is registered."""
         from collective.tiles.advancedstatic.interfaces import (
-            ICollectiveTilesAdvancedstaticLayer)
+            ICollectiveTilesAdvancedstaticLayer,
+        )
         from plone.browserlayer import utils
-        self.assertIn(ICollectiveTilesAdvancedstaticLayer, utils.registered_layers())
+
+        self.assertIn(
+            ICollectiveTilesAdvancedstaticLayer, utils.registered_layers()
+        )
 
 
 class TestUninstall(unittest.TestCase):
@@ -35,16 +52,28 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
         self.installer.uninstallProducts(['collective.tiles.advancedstatic'])
 
     def test_product_uninstalled(self):
         """Test if collective.tiles.advancedstatic is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            'collective.tiles.advancedstatic'))
+        self.assertFalse(
+            self.installer.isProductInstalled(
+                'collective.tiles.advancedstatic'
+            )
+        )
 
     def test_browserlayer_removed(self):
         """Test that ICollectiveTilesAdvancedstaticLayer is removed."""
-        from collective.tiles.advancedstatic.interfaces import ICollectiveTilesAdvancedstaticLayer
+        from collective.tiles.advancedstatic.interfaces import (
+            ICollectiveTilesAdvancedstaticLayer,
+        )
         from plone.browserlayer import utils
-        self.assertNotIn(ICollectiveTilesAdvancedstaticLayer, utils.registered_layers())
+
+        self.assertNotIn(
+            ICollectiveTilesAdvancedstaticLayer, utils.registered_layers()
+        )
+
